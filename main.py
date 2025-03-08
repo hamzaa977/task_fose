d919e7fa44b8be7b7c164dffc148b141cc05a358
hamzaa977 <hamzasajjad097@gmail.com>
2025-03-08 16:11:26 +0500

    HamzaSajjad 8-March-2025 , Resolved

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("This is Versioning Practically")
-print("Fine , But eik post to banti ha xD , Now git will become my skill set but i wont do chichorapan on linkedin but add it as skill.")
+print("Fine , But eik post to banti ha xD")
+print("Now git will become my skill set but i wont do chichorapan on linkedin but add it as skill.")
 
 #Resolved by just Combining two lines of code
